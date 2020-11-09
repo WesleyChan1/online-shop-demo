ad8c8c5967af09a5fb055e9051cd9cae075a8251
F2WesleyC <wesley.chan@futuresquared.com.au>
2020-11-09 15:50:29 +1100

    react project created

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+
+        <div className="container">
+            <div className="row text-center">
+                <div className="col-6">
+                    <h1>test</h1>
+                </div>
+                <div className="col-6">
+                    <h1>test</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
